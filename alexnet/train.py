9214200115577a4ce0eda06036ae90621b9e49ac
xingweih <xingweih@163.com>
2017-11-08 18:23:01 +0800

    alexnet 11.8 pm

--- alexnet/train.py
+++ alexnet/train.py
@@ -3,10 +3,14 @@
 import tensorflow as tf
 import numpy as np 
 
-def fc(x, sizeIn, sizeOut, name, relu=True):
+def fc(x, sizeIn, sizeOut, name, relu=True, weight_bias=None):
 	with tf.variable_scope(name) as scope:
-		weights = tf.get_variable('weights', shape=[sizeIn, sizeOut], trainable=True)
-		biases = tf.get_variable('biases', shape=[sizeOut], trainable=True)
+		if(weight_bias == None):
+			weights = tf.get_variable('weights', shape=[sizeIn, sizeOut], trainable=True)
+			biases = tf.get_variable('biases', shape=[sizeOut], trainable=True)
+		else:
+			weights = weight_bias[name][0]
+			biases = weight_bias[name][1]
 		act = tf.nn.xw_plus_b(x, weights, biases, name=scope.name)
 		if(relu == True):
 			relu = tf.nn.relu(act)
@@ -17,12 +21,16 @@ def fc(x, sizeIn, sizeOut, name, relu=True):
 def dropout(x, keep_prob):
 	return tf.nn.dropout(x, keep_prob)
 
-def inference(images):
+def inference(images, weight_bias):
 	with tf.variable_scope('conv1') as scope:
-		kernel = tf.Variable(tf.truncated_normal([11, 11, 3, 96],
-			dtype = tf.float32, stddev=1e-1), name='weights')
+		if(weight_bias == None):
+			kernel = tf.Variable(tf.truncated_normal([11, 11, 3, 96],
+				dtype = tf.float32, stddev=1e-1), name='weights')
+			biases = tf.Variable(tf.constant(0.0, shape=[96], dtype=tf.float32), name='biases')
+		else:
+			kernel = weight_bias['conv1'][0]
+			biases = weight_bias['conv1'][1]
 		conv = tf.nn.conv2d(images, kernel, [1, 4, 4, 1], padding='VALID')
-		biases = tf.Variable(tf.constant(0.0, shape=[96], dtype=tf.float32), name='biases')
 		bias = tf.nn.bias_add(conv, biases)
 		conv1 = tf.nn.relu(bias, name=scope)
 
@@ -34,10 +42,14 @@ def inference(images):
 			beta=0.75, depth_radius=2, bias=2.0)
 	
 	with tf.variable_scope('conv2') as scope:
-		kernel = tf.Variable(tf.truncated_normal([5, 5, 96, 256],
-			dtype = tf.float32, stddev=1e-1), name='weights')
+		if(weight_bias == None):
+			kernel = tf.Variable(tf.truncated_normal([5, 5, 96, 256],
+				dtype = tf.float32, stddev=1e-1), name='weights')
+			biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), name='biases')
+		else:
+			kernel = weight_bias['conv2'][0]
+			biases = weight_bias['conv2'][1]
 		conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
-		biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), name='biases')
 		bias = tf.nn.bias_add(conv, biases)
 		conv2 = tf.nn.relu(bias, name=scope)
 
@@ -49,26 +61,38 @@ def inference(images):
 			beta=0.75, depth_radius=2, bias=2.0)
 
 	with tf.variable_scope('conv3') as scope:
-		kernel = tf.Variable(tf.truncated_normal([3, 3, 256, 384],
-			dtype = tf.float32, stddev=1e-1), name='weights')
+		if(weight_bias == None):
+			kernel = tf.Variable(tf.truncated_normal([3, 3, 256, 384],
+				dtype = tf.float32, stddev=1e-1), name='weights')
+			biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32), name='biases')
+		else:
+			kernel = weight_bias['conv3'][0]
+			biases = weight_bias['conv3'][1]
 		conv = tf.nn.conv2d(lrn2, kernel, [1, 1, 1, 1], padding='SAME')
-		biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32), name='biases')
 		bias = tf.nn.bias_add(conv, biases)
 		conv3 = tf.nn.relu(bias, name=scope)
 	
 	with tf.variable_scope('conv4') as scope:
-		kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 384],
-			dtype = tf.float32, stddev=1e-1), name='weights')
+		if(weight_bias == None):
+			kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 384],
+				dtype = tf.float32, stddev=1e-1), name='weights')
+			biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32), name='biases')
+		else:
+			kernel = weight_bias['conv4'][0]
+			biases = weight_bias['conv4'][1]
 		conv = tf.nn.conv2d(conv3, kernel, [1, 1, 1, 1], padding='SAME')
-		biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32), name='biases')
 		bias = tf.nn.bias_add(conv, biases)
 		conv4 = tf.nn.relu(bias, name=scope)
 
 	with tf.variable_scope('conv5') as scope:
-		kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 256],
-			dtype = tf.float32, stddev=1e-1), name='weights')
+		if(weight_bias == None):
+			kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 256],
+				dtype = tf.float32, stddev=1e-1), name='weights')
+			biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), name='biases')
+		else:
+			kernel = weight_bias['conv5'][0]
+			biases = weight_bias['conv5'][1]
 		conv = tf.nn.conv2d(conv4, kernel, [1, 1, 1, 1], padding='SAME')
-		biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32), name='biases')
 		bias = tf.nn.bias_add(conv, biases)
 		conv5 = tf.nn.relu(bias, name=scope)
 
@@ -76,29 +100,36 @@ def inference(images):
 		padding='VALID', name='pool2')
 
 	flatten = tf.reshape(pool5, [-1, 6*6*256])
-	fc6 = fc(flatten, 6*6*256, 4096, name='fc6')
+	fc6 = fc(flatten, 6*6*256, 4096, name='fc6', weight_bias=weight_bias)
+		
 	dropout6 = dropout(fc6, 0.5)
 
-	fc7 = fc(fc6, 4096, 4096, name='fc7')
+	fc7 = fc(fc6, 4096, 4096, name='fc7', weight_bias=weight_bias)
 	dropout7 = dropout(fc7, 0.5)
 
-	fc8 = fc(fc7, 4096, 1000, name='fc7')
+	fc8 = fc(fc7, 4096, 1000, name='fc7', weight_bias=weight_bias)
 	dropout8 = dropout(fc8, 0.5)
 
 def load_initial_params(weight_bias):
 	weights_dict = np.load(weight_bias, encoding='bytes').item()
 	print(weights_dict.keys())
+	return weights_dict
+	#w1 = tf.Variable(weights_dict['conv1'][0])
+	'''
 	for op_name in weights_dict:
 		print(op_name)
 		with tf.variable_scope(op_name, reuse=True) as scope:
 			for data in weights_dict[op_name]:
 				print(scope)
 				if(len(data.shape) == 1):
-					var = tf.get_variable('biases', trainable=False)
+					var = tf.get_variable(name=op_name, trainable=False)
 				else:
-					var = tf.get_variable('weights', trainable=False)
+					var = tf.get_variable(name=op_name, trainable=False)
+	'''
 					
 if __name__ == '__main__':
 	print('begin')
-	load_initial_params('alexnet.npy')
+	image = tf.get_variable('image', shape=[1, 227, 227, 3])
+	weight_bias = load_initial_params('alexnet.npy')
+	inference(image, weight_bias)
 	print('end')
